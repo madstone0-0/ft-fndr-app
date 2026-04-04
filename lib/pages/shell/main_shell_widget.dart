2ef6094a50ff80aaf294fbe407c14b209b49753c
sinaaa-aaam <149313882+sinaaa-aaam@users.noreply.github.com>
2026-04-04 16:56:17 +0000

    Fix import path for bookmarks_widget

--- lib/pages/shell/main_shell_widget.dart
+++ lib/pages/shell/main_shell_widget.dart
@@ -3,7 +3,7 @@ import '/flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../screens/bookmarks_widget.dart';
+import '../pages/bookmarks_widget.dart';
 
 class MainShellWidget extends StatelessWidget {
   const MainShellWidget({
