dd12f1c6da0e45b471853ce5e7e2132d6bb07781
sinaaa-aaam <149313882+sinaaa-aaam@users.noreply.github.com>
2026-04-04 17:12:52 +0000

    Remove stale import

--- lib/pages/shell/main_shell_widget.dart
+++ lib/pages/shell/main_shell_widget.dart
@@ -3,8 +3,6 @@ import '/flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../pages/bookmarks_widget.dart';
-
 class MainShellWidget extends StatelessWidget {
   const MainShellWidget({
     super.key,
